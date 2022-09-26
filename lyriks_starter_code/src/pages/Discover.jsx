6ef812813cb930f21f1826f8e55d36d105d54b84
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-09-27 00:13:53 +0530

    get fetching data and error and loader

--- lyriks_starter_code/src/pages/Discover.jsx
+++ lyriks_starter_code/src/pages/Discover.jsx
@@ -1,13 +1,23 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
-  console.log(genres);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const generTilte = "POP";
+
+  console.log("data===", data);
+
+  if (isFetching) return <Loader title="Loading Songs...." />;
+
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {generTilte}
+        </h2>
         <select
           onChange={() => {}}
           value=""
@@ -22,7 +32,7 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
