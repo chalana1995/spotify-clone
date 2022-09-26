7caa127eed5ed244980e74ce2cf7f33099dbfd7b
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-09-26 14:16:15 +0530

    creayte shazam core

---
+++ lyriks_starter_code/src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9edd9c4f23mshb49e83b66f4cba4p1b5541jsne1a7d9585e26",
+    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+  },
+};
+
+fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+});
